02350fe234a648353d1b6118a4af435cd7dfde54
Joshua Raphael Galvan <joshuaraphael.galvan.iics@ust.edu.ph>
2021-08-23 23:58:28 +0800

    fixed chatfeature

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import HeaderButton from '../components/HeaderButton';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+// import React, { useState } from 'react';
+// import { View, Text, StyleSheet, Button } from 'react-native';
+// import HeaderButton from '../components/HeaderButton';
+// import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-const ChatScreen = props => {
+// const ChatScreen = props => {
 
-    const [isHidden, setIsHidden] = useState(true)
+//     const [isHidden, setIsHidden] = useState(true)
 
-    return (
-        <View style={styles.screen}>
-                {
-                    !isHidden && <Text>SHEEEEESH</Text>
-                }
-                <Button
-                    title={isHidden ? 'Show' : 'Hide'}
-                    onPress={() => {
-                        setIsHidden(!isHidden)
-                    }}
-                />
-        </View>
-    )
-}
+//     return (
+//         <View style={styles.screen}>
+//                 {
+//                     !isHidden && <Text>SHEEEEESH</Text>
+//                 }
+//                 <Button
+//                     title={isHidden ? 'Show' : 'Hide'}
+//                     onPress={() => {
+//                         setIsHidden(!isHidden)
+//                     }}
+//                 />
+//         </View>
+//     )
+// }
 
 // ChatScreen.navigationOptions = navData => {
 //     return {
@@ -38,106 +38,108 @@ const ChatScreen = props => {
 //     };
 // };
 
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
-
-export default ChatScreen
-
-// // @refresh reset
-
-// import React, { useState, useEffect, useCallback } from 'react'
-// import { GiftedChat } from 'react-native-gifted-chat'
-// import AsyncStorage from '@react-native-async-storage/async-storage'
-// import { StyleSheet, TextInput, View, YellowBox, Button, LogBox } from 'react-native'
-// import firebase from '../firebase/config'
-
-
-
-
-// LogBox.ignoreLogs(['Setting a timer for a long period of time'])
-
-// const db = firebase.firestore()
-// const chatsRef = db.collection('chats')
-
-// const ChatScreen = (props) => {
-//     const [user, setUser] = useState(null)
-//     const [name, setName] = useState('')
-//     const [messages, setMessages] = useState([])
-
-//     useEffect(() => {
-//         readUser()
-//         const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
-//             const messagesFirestore = querySnapshot
-//                 .docChanges()
-//                 .filter(({ type }) => type === 'added')
-//                 .map(({ doc }) => {
-//                     const message = doc.data()
-//                     //createdAt is firebase.firestore.Timestamp instance
-//                     //https://firebase.google.com/docs/reference/js/firebase.firestore.Timestamp
-//                     return { ...message, createdAt: message.createdAt.toDate() }
-//                 })
-//                 .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-//             appendMessages(messagesFirestore)
-//         })
-//         return () => unsubscribe()
-//     }, [])
-
-//     const appendMessages = useCallback(
-//         (messages) => {
-//             setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
-//         },
-//         [messages]
-//     )
-
-//     async function readUser() {
-//         const user = await AsyncStorage.getItem('user')
-//         if (user) {
-//             setUser(JSON.parse(user))
-//         }
-//     }
-//     async function handlePress() {
-//         const _id = Math.random().toString(36).substring(7)
-//         const user = { _id, name }
-//         await AsyncStorage.setItem('user', JSON.stringify(user))
-//         setUser(user)
-//     }
-//     async function handleSend(messages) {
-//         const writes = messages.map((m) => chatsRef.add(m))
-//         await Promise.all(writes)
-//     }
-
-//     if (!user) {
-//         return (
-//             <View style={styles.container}>
-//                 <TextInput style={styles.input} placeholder="Enter Name" value={name} onTextInput={setName}></TextInput>
-//                 <Button onPress={handlePress} title="Enter the chat" />
-//             </View>
-//         )
-//     }
-//     return <GiftedChat messages={messages} user={user} onSend={handleSend} />
-// }
-
 // const styles = StyleSheet.create({
-//     container: {
+//     screen: {
 //         flex: 1,
-//         backgroundColor: '#fff',
-//         alignItems: 'center',
 //         justifyContent: 'center',
-//         padding: 30,
-//     },
-//     input: {
-//         height: 50,
-//         width: '100%',
-//         borderWidth: 1,
-//         padding: 15,
-//         marginBottom: 20,
-//         borderColor: 'gray',
-//     },
+//         alignItems: 'center'
+//     }
 // })
 
 // export default ChatScreen
+
+// @refresh reset
+
+import React, { useState, useEffect, useCallback } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { StyleSheet, TextInput, View, YellowBox, Button, LogBox } from 'react-native'
+import 'firebase/firestore'
+import firebase from '../firebase/config'
+
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+// LogBox.ignoreLogs(['Setting a timer for a long period of time'])
+
+const db = firebase.firestore()
+const chatsRef = db.collection('chats')
+
+const ChatScreen = (props) => {
+    const [user, setUser] = useState(null)
+    const [name, setName] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        readUser()
+        const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
+            const messagesFirestore = querySnapshot
+                .docChanges()
+                .filter(({ type }) => type === 'added')
+                .map(({ doc }) => {
+                    const message = doc.data()
+                    //createdAt is firebase.firestore.Timestamp instance
+                    //https://firebase.google.com/docs/reference/js/firebase.firestore.Timestamp
+                    return { ...message, createdAt: message.createdAt.toDate() }
+                })
+                .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+            appendMessages(messagesFirestore)
+        })
+        return () => unsubscribe()
+    }, [])
+
+    const appendMessages = useCallback(
+        (messages) => {
+            setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
+        },
+        [messages]
+    )
+
+    async function readUser() {
+        const user = await AsyncStorage.getItem('user')
+        if (user) {
+            setUser(JSON.parse(user))
+        }
+    }
+    async function handlePress() {
+        const _id = Math.random().toString(36).substring(7)
+        const user = { _id, name }
+        await AsyncStorage.setItem('user', JSON.stringify(user))
+        setUser(user)
+    }
+    async function handleSend(messages) {
+        const writes = messages.map((m) => chatsRef.add(m))
+        await Promise.all(writes)
+    }
+
+    if (!user) {
+        return (
+            <View style={styles.container}>
+                <TextInput style={styles.input} placeholder="Enter Name" value={name} onTextInput={setName}></TextInput>
+                <Button onPress={handlePress} title="Enter the chat" />
+            </View>
+        )
+    }
+    return <GiftedChat messages={messages} user={user} onSend={handleSend} />
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 30,
+    },
+    input: {
+        height: 50,
+        width: '100%',
+        borderWidth: 1,
+        padding: 15,
+        marginBottom: 20,
+        borderColor: 'gray',
+    },
+})
+
+export default ChatScreen
